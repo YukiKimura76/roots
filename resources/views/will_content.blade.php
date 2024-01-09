30096dbaf15050f197e5673600eb866063e4523c
nabe-696 <yuma.nabeshima@gmail.com>
2024-01-09 17:19:37 +0900

    a

---
+++ resources/views/will_content.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>遺言書</title>
+    <!-- スタイルや他の設定 -->
+</head>
+<body>
+    <!-- 遺言書の内容を表示 -->
+    {!! $willContent !!}
+</body>
+</html>
